31475b541623121ad1738920b13d1302d6ac49a1
Christoph <a0706052@unet.univie.ac.at>
2011-05-31 03:38:54 +0200

    Host class Client tutorial Signed-off-by: Christoph <a0706052@unet.univie.ac.at>

--- Code/client.py
+++ Code/client.py
@@ -1,11 +1,15 @@
 # Echo client program
 import socket
 
-HOST = 'daring.cwi.nl'    # The remote host
+HOST = '127.0.0.1'    # The remote host
 PORT = 50007              # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-s.send(b'Hello, world')
-data = s.recv(1024)
-s.close()
-print('Received', repr(data))
+i = 0
+while i < 2:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+    print s.getsockname()
+    #s.send(b'Hello, world')
+    #data = s.recv(1024)
+    s.close()
+    #print('Received', repr(data))
+    i = i + 1
--- Code/host.py
+++ Code/host.py
@@ -1,15 +1,30 @@
-# Echo server program
 import socket
+import sys
 
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 50007              # Arbitrary non-privileged port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-conn, addr = s.accept()
-print('Connected by', addr)
-while True:
-    data = conn.recv(1024)
-    if not data: break
-    conn.send(data)
-conn.close()
+class Host():
+    port = 0
+    clients = list()
+    numClients = 0
+    
+    def __init__(self, port, numClients):
+        self.port = port
+        self.numClients = numClients
+
+    def initConnect(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('localhost', self.port))
+        s.listen(1)
+        i = 0
+        while i < self.numClients:
+            conn, addr = s.accept()
+            self.clients.append([i, addr, conn.getsockname()])
+            i = i + 1
+            print 'Connected by', addr
+            conn.close()
+        print self.clients
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        myServer = Host(long(sys.argv[1]), int(sys.argv[2]))
+        myServer.initConnect()
+    else: print 'No'
